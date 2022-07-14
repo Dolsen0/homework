a1cd1b9720b0a8dcffa752b5b5eafd979f5c54e8
Derek Olsen <derekolsen@Dereks-MacBook-Pro-2.local>
2022-07-14 10:40:13 -0400

    created homework folder

---
+++ day3.js
@@ -0,0 +1,46 @@
+/* 
+~~~~~~
+1. Write a function that adds two numbers together.
+~~~~~~
+*/
+
+
+// function sum(num1, num2){
+//     return(num1+num2)
+// }
+
+// console.log(sum(3,5))
+
+
+/*
+~~~~~~
+2. Write a car object that has a modal and color.  Create a property called year then console.log all of the objects properties individually.
+~~~~~~
+*/
+
+
+// let car = {
+//     color: `white`,
+//     modal: `Subaru`,
+// }
+
+// car.year = 2009
+// console.log(car.year)
+// console.log(car["modal"])
+
+
+
+/*
+~~~~~~
+3. Write a function to get the first element in an array.
+*/
+
+let arr = [`two`, `three`, `four`, `one`,]
+let fruit = [`apple`, `orange`, `kiwi`, `mango`,]
+
+function firstElement(arr){
+    return arr[0]
+}
+
+console.log(firstElement(arr))
+
